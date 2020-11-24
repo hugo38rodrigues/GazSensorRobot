6e06d4eb00905b61331016dd4b5e81a15a0b10d1
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2020-11-24 22:13:45 +0100

    supression de l'ancien code moteur et ajout du code obstacle à finir

---
+++ code_obstacle/code_obstacle.ino
@@ -0,0 +1,39 @@
+int capteurAV=6;
+int capteurAR=7;
+int capteurAVG=8;
+int capteurAVD=9;
+
+void setup(){
+  pinMode(capteurAV, INPUT);
+  pinMode(capteurAR, INPUT);
+  pinMode(capteurAVG, INPUT);
+  pinMode(capteurAVD, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(capteurAV) == HIGH || digitalRead(capteurAVG) == HIGH || digitalRead(capteurAVD) == HIGH)
+    reculer(1);
+  else if (digitalRead(capteurAR) == HIGH)
+    StopMoteur();
+    else
+      avancer();
+    delay(100);
+  }
+  
+// activer le moteur en sens inverse pendant la durée indiquer en parametre
+void reculer(int duree){  
+  // stop moteur 
+  //marche arriere
+  delay(duree)
+  
+}
+// couper le Moteur (desactiver le pont H)
+void StopMoteur()
+{
+  
+}
+// activer le moteur (pont en H sur on)
+void avancer(){
+  
+}
