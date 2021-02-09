38515d73f6acebc918a416de40e45371381d681e
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2021-02-09 13:17:53 +0100

    ajout datasheet MQ7 et traduction des com

--- code_capteur_C0/code_capteur_C0.ino
+++ code_capteur_C0/code_capteur_C0.ino
@@ -1,28 +1,28 @@
-const int AOUTpin=0;  // the AOUT pin of the C0 sensor goes into analog pin A0 of the Arduino
-const int BOUTpin=8;  // the BOUT pin of the C0 sensor goes into analog pin B8 of the Arduino
-const int ledPin=13;  // the anode of the LED connects to difgital pin D13 of the arduino
+const int AOUTpin=0;  // la broche AOUT du capteur C0 va dans la broche analogique A0 de l'Arduino
+const int DOUTpin=8;  // la broche DOUT du capteur C0 va dans la broche analogique D8 de l'Arduino
+const int ledPin=13;  // l'anode de la LED se connecte à la broche D13 de l'Arduino
 
 int limit;
 int value;
 
 void setup() {
-Serial.begin(9600);//sets the baud rate
-pinMode(BOUTpin, INPUT);//sets the pin as an input to the arduino
-pinMode(ledPin, OUTPUT);//sets the pin as an output to the arduino
+Serial.begin(9600);//fixe le débit en bauds
+pinMode(DOUTpin, INPUT);//définit la broche comme une entrée pour l'arduino
+pinMode(ledPin, OUTPUT);//met la broche en sortie de l'arduino
 }
 
 void loop() {
-value= analogRead(AOUTpin);//reads the analog valuefrom the CO sensor's AOUT pin
-limit= digitalRead(BOUTpin);//reads the digital value from the CO sensor's BOUT pin
+value= analogRead(AOUTpin);//lit la valeur analogique à partir de la broche AOUT du capteur de CO
+limit= digitalRead(DOUTpin);//lit la valeur numérique à partir de la broche DOUT du capteur de CO
 Serial.print("C0 value: ");
-Serial.println(value);//prints the C0 value
+Serial.println(value);//imprime la valeur C0
 Serial.print("Limit: ");
-Serial.println(limit);//prints the limit reached as either LOW or HIGH (above or underneath)
+Serial.println(limit);//imprime la limite atteinte comme BASSE ou HAUTE (au-dessus ou en dessous)
 delay(100);
 if (limit == HIGH){
-  Serial.print("Allarm");//if  limit has been reached
+  Serial.print("Allarm");//si la limite a été atteinte
 }
 else{
-  Serial.print("OK"); //if threshold not reched
+  Serial.print("OK"); //si le seuil n'est pas revu à la baisse
 }
 }
